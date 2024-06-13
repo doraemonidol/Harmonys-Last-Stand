79fd9e175ae9393721c8dc0d368c5f6405702d87
meow-bland <87884396+doraemonidol@users.noreply.github.com>
2024-06-13 13:46:34 +0700

    Game Prototype

---
+++ Assets/GabrielAguiarProductions/Unique_Projectiles_Volume_1/Scripts/UniqueProjectiles/RotateToMouseScript.cs
@@ -0,0 +1,68 @@
+﻿//
+//NOTES:
+//This script is used for DEMONSTRATION porpuses of the Projectiles. I recommend everyone to create their own code for their own projects.
+//This is just a basic example.
+//
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateToMouseScript : MonoBehaviour {
+
+	public float maximumLenght;
+
+	private Ray rayMouse;
+	private Vector3 pos;
+	private Vector3 direction;
+	private Quaternion rotation;
+	[SerializeField] private LayerMask groundMask;
+	[SerializeField] private GameObject directionIndicator;
+	[SerializeField] private Camera cam;
+	private WaitForSeconds updateTime = new WaitForSeconds (0.01f); 
+
+	void Start () {
+		cam = Camera.main;
+	}
+
+	public void StartUpdateRay (){
+		StartCoroutine (UpdateRay());
+	}
+
+	IEnumerator UpdateRay (){
+		if (cam) {
+			RaycastHit hit;
+			var mousePos = Input.mousePosition;
+			var ray = cam.ScreenPointToRay(mousePos);
+			
+			if (Physics.Raycast(ray, out hit, Mathf.Infinity, groundMask)) {
+				mousePos = hit.point;
+			} else {
+				mousePos = ray.GetPoint(maximumLenght);
+			}
+			
+			Debug.Log(mousePos);
+			
+			RotateToMouse (directionIndicator, mousePos);
+			yield return updateTime;
+			StartCoroutine (UpdateRay ());
+		} else
+			Debug.Log ("Camera not set");
+	}
+
+	public void RotateToMouse (GameObject obj, Vector3 destination ) {
+		direction = destination - obj.transform.position;
+		direction.y = 0;
+		
+		rotation = Quaternion.LookRotation (direction);
+		obj.transform.forward = direction;
+	}
+
+	public Vector3 GetDirection () {
+		return direction;
+	}
+
+	public Quaternion GetRotation () {
+		return rotation;
+	}
+}
---
+++ Assets/GabrielAguiarProductions/Unique_Projectiles_Volume_1/Scripts/UniqueProjectiles/SpawnProjectilesScript.cs
@@ -0,0 +1,123 @@
+﻿//
+//NOTES:
+//This script is used for DEMONSTRATION porpuses of the Projectiles. I recommend everyone to create their own code for their own projects.
+//This is just a basic example.
+//
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpawnProjectilesScript : MonoBehaviour {
+
+    public bool useTarget;
+	public bool use2D;
+	public bool cameraShake;
+	// public Text effectName;
+	public RotateToMouseScript rotateToMouse;
+	public GameObject firePoint;
+    public GameObject target;
+    public List<GameObject> VFXs = new List<GameObject> ();
+
+	private int count = 0;
+	private float timeToFire = 0f;
+	private GameObject effectToSpawn;
+	[SerializeField] private Camera mainCamera;
+
+	void Start () {
+
+		if(VFXs.Count>0)
+			effectToSpawn = VFXs[0];
+		else
+			Debug.Log ("Please assign one or more VFXs in inspector");
+		
+		// if (effectName != null) effectName.text = effectToSpawn.name;
+
+		rotateToMouse.StartUpdateRay ();
+        if (useTarget && target != null)
+        {
+            var collider = target.GetComponent<BoxCollider>();
+            if (!collider)
+            {
+                target.AddComponent<BoxCollider>();
+            }
+        }
+    }
+
+	void Update () {
+		if (Input.GetMouseButton (0) && Time.time >= timeToFire) {
+			timeToFire = Time.time + 1f / effectToSpawn.GetComponent<ProjectileMoveScript>().fireRate;
+			SpawnVFX ();	
+		}
+
+		if (Input.GetKeyDown (KeyCode.Q))
+			Next ();
+		if (Input.GetKeyDown(KeyCode.E))
+			Previous();
+	}
+
+	public void SpawnVFX () {
+		GameObject vfx;
+
+		var cameraShakeScript = mainCamera.GetComponent<CameraShakeSimpleScript> ();
+
+		if (cameraShake && cameraShakeScript)
+			cameraShakeScript.ShakeCamera ();
+
+		if (firePoint) {
+			vfx = Instantiate (effectToSpawn, firePoint.transform.position, Quaternion.identity);
+            if (!useTarget)
+            {
+                if (rotateToMouse)
+                {
+                    vfx.transform.localRotation = rotateToMouse.GetRotation();
+                }
+                else Debug.Log("No RotateToMouseScript found on firePoint.");
+            }
+            else
+            {
+                if (target)
+                {                    
+                    vfx.GetComponent<ProjectileMoveScript>().SetTarget(target, rotateToMouse);
+                    rotateToMouse.RotateToMouse(vfx, target.transform.position);                    
+                }
+                else
+                {
+                    Destroy(vfx);
+                    Debug.Log("No target assigned.");
+                }
+            }
+		}
+		else
+			vfx = Instantiate (effectToSpawn);		
+	}
+
+	public void Next () {
+		count++;
+
+		if (count > VFXs.Count)
+			count = 0;
+
+		for(int i = 0; i < VFXs.Count; i++){
+			if (count == i)	effectToSpawn = VFXs [i];
+			// if (effectName != null)	effectName.text = effectToSpawn.name;
+		}
+	}
+
+	public void Previous () {
+		count--;
+
+		if (count < 0)
+			count = VFXs.Count;
+
+		for (int i = 0; i < VFXs.Count; i++) {
+			if (count == i) effectToSpawn = VFXs [i];
+			// if (effectName )	effectName.text = effectToSpawn.name;
+		}
+	}
+
+	public void CameraShake () {
+		cameraShake = !cameraShake;
+	}
+}
