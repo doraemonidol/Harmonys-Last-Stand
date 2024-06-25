7816037c2088faad74b32c4878c796ce6dccec23
Nguyen Phuc Bao Uyen <95065083+Hollyuyen@users.noreply.github.com>
2024-06-25 12:00:32 +0700

    Up character

--- Assets/Scripts/PlayerDirection.cs
+++ Assets/Scripts/PlayerDirection.cs
@@ -8,24 +8,32 @@ public class PlayerDirection : MonoBehaviour
     [SerializeField] private GameObject directionIndicator;
     private Vector3 _mousePosition;
     [SerializeField] private Camera mainCamera;
-    
+
     void Start()
     {
-        mainCamera = Camera.main;
+        if (mainCamera == null)
+        {
+            mainCamera = Camera.main;
+        }
     }
-    
+
     void Update()
     {
         Vector3 mousePosition = Input.mousePosition;
         _mousePosition = mainCamera.ScreenToWorldPoint(new Vector3(mousePosition.x, mousePosition.y, mainCamera.transform.position.y - transform.position.y));
         _mousePosition.y = 0;
-        // Debug.Log(_mousePosition);
+        
+        // Check for mouse click
+        if (Input.GetMouseButtonDown(0))
+        {
+            RotatePlayer();
+        }
     }
-    
-    private void FixedUpdate()
+
+    private void RotatePlayer()
     {
         Vector3 lookDirection = _mousePosition - transform.position;
         float angle = Mathf.Atan2(lookDirection.z, lookDirection.x) * Mathf.Rad2Deg - 90f;
-        directionIndicator.transform.rotation = Quaternion.Euler(0, -angle, 0);
+        transform.rotation = Quaternion.Euler(0, -angle, 0);
     }
 }
